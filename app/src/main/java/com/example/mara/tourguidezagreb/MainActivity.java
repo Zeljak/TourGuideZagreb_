ca52e10785bcfc7266157ce78f35edf43a4f44ba
Zeljak <zeljka_raic@yahoo.com>
2017-06-05 23:13:38 +0200

    new line

--- app/src/main/java/com/example/mara/tourguidezagreb/MainActivity.java
+++ app/src/main/java/com/example/mara/tourguidezagreb/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mara.tourguidezagreb;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
 
 
 public class MainActivity extends AppCompatActivity {
